52fa0348eed5cb3350f8e8680a8a99a19a528d95
Islambek Khodzhaev <khodzhaevislambek@gmail.com>
2024-12-30 05:21:34 +0200

    third

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import PizzaBlock from "./components/PizzaBlock";
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
             <PizzaBlock title="Узбекская пицца" price={4500} />
-            <PizzaBlock title="Мексиканская пицца" price="450" />
+            <PizzaBlock title="Хз пицца" price="450" />
             <PizzaBlock title="Мексиканская пицца" price="450" />
             <PizzaBlock title="Мексиканская пицца" price="450" />
             <PizzaBlock title="Мексиканская пицца" price="450" />
